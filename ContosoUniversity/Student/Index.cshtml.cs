08723306301005b7a3e097f164a4e56d7d56cf33
j.wozniak <j.wozniak@SJT-D-ICTSUP3>
2023-04-04 14:37:08 +0100

    Add project files.

---
+++ ContosoUniversity/Student/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Student
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Students != null)
+            {
+                Student = await _context.Students.ToListAsync();
+            }
+        }
+    }
+}
